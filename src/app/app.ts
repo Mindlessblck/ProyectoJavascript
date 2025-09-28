7f6683bc448cf3be4580bf8dc5e6c9f6a58ccdee
jeferRH <jefersonkof122@gmail.com>
2025-09-27 21:55:26 -0500

    Mejoré un poco el diseño del header

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Navbar } from "./navegacion/navbar/navbar";
+import { Header } from './component/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet,Header],
   templateUrl: './app.html',
   styleUrls: ['./app.css'] // Cambiado de styleUrl a styleUrls
 })
